47d03df2ebce0df0ff0fb82e69972ed19fbe38f7
jmaghirang <70534153+jmaghirang@users.noreply.github.com>
2024-02-05 11:41:11 -0800

    added js and css

---
+++ Assets/JS/app.js
@@ -0,0 +1,46 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// Scroll to section functionality for smooth scrolling
+navLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - nav.offsetHeight, // Consider the fixed navbar height
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
